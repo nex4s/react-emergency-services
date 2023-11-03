c5cc6df5e2953513c21761dbf5c7b85ccb0ec262
Nexus <129723215+nex4s@users.noreply.github.com>
2023-11-03 10:11:59 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Country from './components/country'
+
+export default function App() {
+  return (
+    <html lang='en' className='h-full sm:h-screen md:h-screen lg:h-screen bg-black'>
+    <Country></Country>
+    </html>
+  )
+}
